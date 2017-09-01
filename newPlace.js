159f5d56568df4e9242a314440899bbaa3aecfd1
Geoffroy Lesage <gefthefrench@gmail.com>
2017-09-01 23:58:11 +0200

    move parameters that we'll never change

--- newPlace.js
+++ newPlace.js
@@ -4,28 +4,26 @@ var place = {
     key: process.env.GOOGLE_API_KEY,
     lat: 30.417652,
     lng: -91.180473,
-    acc: 50,
     name: 'Cypress Hall',
     address: "Cypress Hall, Baton Rouge, LA, 70808",
     type: 'school',
     phone: "",
     website: 'http://www.google.com.au/',
-    lang: 'en-US'
 }
-const places = new GooglePlaces(place.key);
 
+const places = new GooglePlaces(place.key);
 const params = {
     "location": {
         "lat": place.lat,
         "lng": place.lng
     },
-    "accuracy": place.acc,
+    "accuracy": 50,
     "name": place.name,
     "phone_number": place.phone,
     "address": place.address,
     "types": [place.type],
     "website": place.website,
-    "language": place.lang
+    "language": 'en-US'
 }
 
 places.add(params).then(function(res) {
